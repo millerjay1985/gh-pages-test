21a86403711c9f2d7a591199e605503ded3ca855
millerjay1985 <username@users.noreply.github.com>
2016-03-22 15:49:52 -0400

    changed js

--- my_script.js
+++ my_script.js
@@ -1,7 +1,10 @@
-$("button").clcik(function()){
-	$.getJSON("json_data.json", function(obj)){
-		$.each(obj, function(key, value){
-			$("ul").append("<li>"+value.name+"</li>")
-		});
+
+$(document).ready(function() {
+	$("#getMessage").click(function()){
+		$.getJSON("json_data.json", function(obj)){
+			$.each(obj, function(key, value){
+				$(".message").html(value.name)
+			});
+		}
 	}
 }
