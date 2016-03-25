5cefde84af0014604d82995c8a6fb014fb21991c
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-25 15:49:51 -0400

    Update json_data.json.js

--- json_data.json.js
+++ json_data.json.js
@@ -1,1 +1,3 @@
-myFunction([{"author":"jon","quote":"life's a bitch"},{"author":"Lexie","quote":"what's good pimp?"}]);
+myFunction([{"author":"jon","quote":"life's a bitch"},{"author":"Lexie","quote":"what's good pimp?"},
+{"author": "Vonnie","quote":"It must not be meant for you"}, {"author":"Jason", "quote":"We can do it!"},
+{"author":"Shannon","quote":"Y'all need to get the hell out"}]);
