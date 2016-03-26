89922998d36c9df0f789e9f283e8e6b8ca44abf2
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-25 22:59:22 -0400

    Update json_data.json.js

--- json_data.json.js
+++ json_data.json.js
@@ -1,7 +1,7 @@
 myFunction([{"author":"Jon","quote":'"Life is what you make it."'},{"author":"Lexie","quote":'"Go! Fight! Win!!"'},
 {"author": "Vonnie","quote":'"Okay babe."'},{"author":"Jason", "quote":'"I don\'t even got it..."'},
 {"author":"Shannon","quote":'"Y\'all need to get the hell out!"'},{"author":"Greg","quote":'"Y\'all know I\'m just playing!"'},
-{"author":"Amari","quote":'"My \'fava\' work at the White House!"'},{"author":"Aidan","quote":'"I\'ts froze outside!"'},
+{"author":"Amari","quote":'"My \'fava\' work at the White House!"'},{"author":"Aidan","quote":'"It\'s froze outside!"'},
 {"author":"Jordan","quote":'"Daddy want to see me run faster?!"'},{"author":"Earl","quote":'"What\'s up shorty?!"'},
 {"author":"Gwennie","quote":'"Greg if you dont\'t shut the hell up!"'},{"author":"Mickey","quote":'"Hey y\'all!"'},
 {"author":"Pooh","quote":'"I\'m about to stop drinking soda..."'},{"author":"Stanky","quote":'"Hey Mr. Chris where\'s Aidan?"'},
