a7295f2c75155be0a0bcc347090c63d4e0d57047
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-25 23:55:59 -0400

    Update my_script.js

--- my_script.js
+++ my_script.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   //triggers ajax method once new quote button is pushed
   var item;
-  var quote = $(".message.blockquote").html();
+  var quote = $(".quotes").html();
   var author;
   $('#getMessage').click(function(){ 
     //ajax method allows us to use JSONP with git-pages
