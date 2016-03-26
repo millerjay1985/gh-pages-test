7c6c8d306d9a6110fd7f58e7430d838d11a7bd30
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-25 22:49:02 -0400

    Update json_data.json.js

--- json_data.json.js
+++ json_data.json.js
@@ -2,7 +2,7 @@ myFunction([{"author":"Jon","quote":'"Life is what you make it."'},{"author":"Le
 {"author": "Vonnie","quote":'"Okay babe."'},{"author":"Jason", "quote":'"I don\'t even got it..."'},
 {"author":"Shannon","quote":'"Y\'all need to get the hell out!"'},{"author":"Greg","quote":'"Y\'all know I\'m just playing!"'},
 {"author":"Amari","quote":'"My \'fava\' work at the White House!"'},{"author":"Aidan","quote":'"I\'ts froze outside!"'},
-{"author":"Jordan","quote":'"Daddy want to see me run faster?!"'},{"auhtor":"Earl","quote":'"What\'s up shorty?!"'},
-{"author":"Gwennie","quote":'"Greg if you dont\'t shut the hell up!"'}/*,{"author":"Mickey","quote":"Hey y\'all!"},
+{"author":"Jordan","quote":'"Daddy want to see me run faster?!"'},{"author":"Earl","quote":'"What\'s up shorty?!"'},
+{"author":"Gwennie","quote":'"Greg if you dont\'t shut the hell up!"'},{"author":"Mickey","quote":'"Hey y\'all!"'},
 {"author":"Pooh","quote":'"I\'m about to stop drinking soda..."'},{"author":"Stanky","quote":'"Hey Mr. Chris where\'s Aidan?"'},
-{"author":"Dante",'"I got the best M.I.L. in the world!"'}*/]);
+{"author":"Dante",'"I got the best M.I.L. in the world!"'}]);
