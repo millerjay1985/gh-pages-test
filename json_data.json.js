47c67226773bf7e9ba0bda58a1201aec9891679e
Jon Miller <millerjay1985@users.noreply.github.com>
2016-03-25 22:46:32 -0400

    Update json_data.json.js

--- json_data.json.js
+++ json_data.json.js
@@ -3,6 +3,6 @@ myFunction([{"author":"Jon","quote":'"Life is what you make it."'},{"author":"Le
 {"author":"Shannon","quote":'"Y\'all need to get the hell out!"'},{"author":"Greg","quote":'"Y\'all know I\'m just playing!"'},
 {"author":"Amari","quote":'"My \'fava\' work at the White House!"'},{"author":"Aidan","quote":'"I\'ts froze outside!"'},
 {"author":"Jordan","quote":'"Daddy want to see me run faster?!"'},{"auhtor":"Earl","quote":'"What\'s up shorty?!"'},
-{"author":"Gwennie","quote":'"Greg if you dont\'t shut the hell up!"'},{"author":"Mickey","quote":"Hey y\'all!"},
+{"author":"Gwennie","quote":'"Greg if you dont\'t shut the hell up!"'}/*,{"author":"Mickey","quote":"Hey y\'all!"},
 {"author":"Pooh","quote":'"I\'m about to stop drinking soda..."'},{"author":"Stanky","quote":'"Hey Mr. Chris where\'s Aidan?"'},
-{"author":"Dante",'"I got the best M.I.L. in the world!"'}]);
+{"author":"Dante",'"I got the best M.I.L. in the world!"'}*/]);
